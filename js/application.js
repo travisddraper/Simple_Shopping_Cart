a8919c009508cb71364e8a368528af25bc20f08a
Travis Draper <tdanieldraper@gmail.com>
2021-03-08 14:44:55 -0500

    edit

---
+++ js/application.js
@@ -0,0 +1,7 @@
+//inside of the ready function of jQuery, I need to run a price update
+//that appends to each <tr> a $$ amount based on the value inputed to
+//the quantity field
+
+//Need to create a "Cancel" button that will remove its <tr> and need
+//to make sure I add this on the document.body so it is appended to each
+//<tr> that is created and not just the ones that exist.
